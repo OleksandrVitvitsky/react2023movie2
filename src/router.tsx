821a966b231f274b18082fc9e301b5529fa77095
OleksandrVitvitsky <vitvitsky.oleksandr@gmail.com>
2024-03-27 11:31:46 +0200

    add finish 99.8

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,7 @@
 import {createBrowserRouter, Navigate} from "react-router-dom";
 import {MainLayout} from "./layots";
 import {MoviesPage,MovieDetailsPage} from "./pages";
-import {useAppDispatch} from "./hooks/useAppDispatch";
+
 
 const router = createBrowserRouter([
     {
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
                 path: 'movies', element: <MoviesPage/>
             },
             {
-                path: '/movies/:id', element:  <MovieDetailsPage/>//, loader:({params:{id}})=>userService.getById(id)
+                path: '/movies/:id', element:  <MovieDetailsPage/>
             }
 
         ]
